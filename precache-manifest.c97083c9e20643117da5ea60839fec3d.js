b80218a5b1242e35393e934a102f49d3bb16300d
Taofeeq Oluderu <oluderutaofeeq@gmail.com>
2020-03-22 02:25:45 +0100

    Updates

---
+++ precache-manifest.c97083c9e20643117da5ea60839fec3d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2889476ac70d3460a171bb20fb867979",
+    "url": "/StaffingSystem/index.html"
+  },
+  {
+    "revision": "8da0026fe89b1b31609e",
+    "url": "/StaffingSystem/static/css/2.5bc09813.chunk.css"
+  },
+  {
+    "revision": "2428f31d86eb4737c5c1",
+    "url": "/StaffingSystem/static/css/main.60332307.chunk.css"
+  },
+  {
+    "revision": "8da0026fe89b1b31609e",
+    "url": "/StaffingSystem/static/js/2.b3821a96.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/StaffingSystem/static/js/2.b3821a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2428f31d86eb4737c5c1",
+    "url": "/StaffingSystem/static/js/main.2991fece.chunk.js"
+  },
+  {
+    "revision": "fcb4583b3495ebfc4d12",
+    "url": "/StaffingSystem/static/js/runtime-main.31049cbc.js"
+  }
+]);
